bc079f9da27c1fa9abcde7de0d1192c288f7a7fc
thaibmt <thaibmt47@gmail.com>
2023-12-08 23:12:56 +0700

    update

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const browserObject = require('./browser');
 const scraperController = require('./pageController');
+const cron = require('node-cron');
 
-//Start the browser and create a browser instance
-let browserInstance = browserObject.startBrowser();
+const main = function () {
+    //Start the browser and create a browser instance
+    let browserInstance = browserObject.startBrowser();
 
-// Pass the browser instance to the scraper controller
-scraperController(browserInstance)
+    // Pass the browser instance to the scraper controller
+    scraperController(browserInstance)
+}
+cron.schedule('*/5 * * * *', () => {
+    console.log('Cron job is running every 10 minutes.');
+    main();
+    // Thêm các thao tác bạn muốn thực hiện sau mỗi 10 phút ở đây
+});
